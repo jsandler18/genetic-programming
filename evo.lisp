0b99998d27ddf0eb79f7ddcea455316c9291f681
jsandler18 <jsandler18@gmail.com>
2015-07-28 19:58:58 -0400

    function to create first generation working

--- evo.lisp
+++ evo.lisp
@@ -8,14 +8,15 @@
 	(if (> arg1 arg2) 1 -1)
 )
 ;;; stuff pertaining to generating program trees for the initial population
-(defun full (funcs terms level size)
-	"grows a full program tree"
+(defun full (funcs terms level size maxargs)
+	"creates a program tree using the full method and recursive preorder traversal"
 	(if (/= level size)
-		(CONS (NTH (RANDOM (LENGTH FUNCs)) funcs) `(,(full funcs terms (+ level 1) size) ,(full funcs terms (+ level 1) size)))
+		(CONS (NTH (RANDOM (LENGTH FUNCs)) funcs) `(,(full funcs terms (+ level 1) size maxargs) ,(full funcs terms (+ level 1) size maxargs)))
 		(nth (random (length terms)) terms)
 	)
 )
-(defun grow (funcs terms level size)
+(defun grow (funcs terms level size maxargs)
+	"creates a program tree using the grow method and recursive preorder traversal"
 	;pick a node randomly from set of functions and terminals
 	(setq funcsandterms (append funcs terms))
 	(setq node (random (length funcsandterms)))
@@ -23,11 +24,11 @@
 		;if not at max size
 		(if (= 1 level)
 			;if at root, make a function
-			(CONS (NTH (RANDOM (LENGTH FUNCs)) funcs) `(,(grow funcs terms (+ level 1) size) ,(grow funcs terms (+ level 1) size)))
+			(CONS (NTH (RANDOM (LENGTH FUNCs)) funcs) `(,(grow funcs terms (+ level 1) size maxargs) ,(grow funcs terms (+ level 1) size maxargs)))
 			;if not at root, choose node from terminals and functions
 			(if (< node (length funcs))
 				;if an operator, generate children 
-				(CONS (NTH node funcs) `(,(grow funcs terms (+ level 1) size) ,(grow funcs terms (+ level 1) size)))
+				(CONS (NTH node funcs) `(,(grow funcs terms (+ level 1) size maxargs) ,(grow funcs terms (+ level 1) size maxargs)))
 				;if a terminal, put terminal
 				(nth node funcsandterms)
 			)
@@ -36,8 +37,30 @@
 		(nth (random (length terms)) terms)
 	)	
 )
+(defun rhah (funcs terms popsize maxsize maxargs)
+	"creates an initial generation of programs using the 'ramped half-and-half' method (thus rhah)"
+	(setq result ()) ;an initial empty list
+	(setq numpersize (/ popsize (- maxsize 1))) ;sets number of programs per size to be equal percentages
+	(loop for size from 2 to maxsize ; for each program size
+		do(
+			loop for program from 1 to numpersize ;for each program of this size
+				do (
+					if (= (mod program 2) 0) ;alternate between full and grow methods
+						(setq result (cons (full funcs terms 1 size maxargs) result))
+						(setq result (cons (grow funcs terms 1 size maxargs) result))
+				)
+		)	
+	)
+	result
+	
+)
+
 
 (setq functions '(+ - * %))
 (setq terminals '(1 2 3 4 5 6 7 8 9))
 
-(print (full functions terminals 1 4))
+(setq a (rhah functions terminals 10 6))
+(dotimes (n 10)
+	
+	print(nth n a)
+)
