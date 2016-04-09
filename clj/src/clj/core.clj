dec9a0e3281462aee98d4296d5bf86e87d3eb851
Everest Liu <everest.fuboliu@gmail.com>
2016-04-09 16:30:26 -0400

    Added pick-individual

--- clj/src/clj/core.clj
+++ clj/src/clj/core.clj
@@ -1,40 +1,39 @@
 (ns clj.core
   (:gen-class
-   :name Core ))
-
+:name Core ))
 
 (defn full [functions function-args terminals level maxlevels]
-  "Creates a program tree that is a full tree (all terminal leaves are on the same level)."
-  ;gets a random index in the function vector
+"Creates a program tree that is a full tree (all terminal leaves are on the same level)."
+;gets a random index in the function vector
   (let [rnd-func-idx (rand-int (count functions))]
     (if (not= level maxlevels)
-      ;preappend the randomly picked function to a list of arguments
+;preappend the randomly picked function to a list of arguments
       (cons (nth functions rnd-func-idx) (loop [args 0 arg-count (nth function-args rnd-func-idx) arg-list '()] 
         (if (not= args arg-count)
-          ;make a full tree of one level smaller and preappend that to the argument list
+;make a full tree of one level smaller and preappend that to the argument list
           (recur (+ 1 args) arg-count (cons (full functions function-args terminals (+ 1 level) maxlevels) arg-list))
            arg-list)))
-      ;return one of the terminals at the bottom level
+;return one of the terminals at the bottom level
       (nth terminals (rand-int (count terminals))))))
-
+      
 (defn grow [functions function-args terminals level maxlevels]
-  "Creates a programm tree by 'growing it' from the root and has randomness to the breadth and depth"
-  ;if anot at bottom, grow
+"Creates a programm tree by 'growing it' from the root and has randomness to the breadth and depth"
+;if anot at bottom, grow
   (if (not= level maxlevels)
-    ;if the root level or if a random int is above 1 (80% chance), must be a function
+;if the root level or if a random int is above 1 (80% chance), must be a function
     (if (or (= level 1) (> (rand-int 9) 1))
       (let [rnd-func-idx (rand-int (count functions))]
-        ;preappend the chosen function to the arguemnt list
+;preappend the chosen function to the arguemnt list
         (cons (nth functions rnd-func-idx) (loop [args 0 arg-count (nth function-args rnd-func-idx) arg-list '()]
           (if (not= args arg-count)
-            ;generate the argument list by recurively calling grow
+;generate the argument list by recurively calling grow
             (recur (+ 1 args) arg-count (cons (grow functions function-args terminals (+ 1 level) maxlevels) arg-list))
             arg-list))))
       (nth terminals (rand-int (count terminals))))
     (nth terminals (rand-int (count terminals)))))
-
+    
 (defn rhah [functions function-args terminals population-size max-tree-size]
-  "creates an initial generation using 'ramped half and half'.  Ramped refers to how it starts with small trees and increases the tree sizes and half and half refers to using grow and full equally"
+"creates an initial generation using 'ramped half and half'.  Ramped refers to how it starts with small trees and increases the tree sizes and half and half refers to using grow and full equally"
   (let [programs-per-size (/ population-size (- max-tree-size 1))] ;divide populationsize by number of possible sizes for number of programs of that size per tree
     (loop [size 2 program-list '[]] ;loop while size is less than max
       (if (<= size max-tree-size)
@@ -45,23 +44,44 @@
               (full functions function-args terminals 1 size)) inner-list))
             inner-list)) program-list))) ;concat the list of programs generated for that size with the rest of the generated programs
         program-list)))) 
-
+        
 (defrecord ProgramFitness [program raw standardized adjusted normalized])
 
 (defn gen-zero [functions function-args terminals population-size max-tree-size]
-  "creates a vector of program-fitness structures and fills the program field with programs generated from rhah"
+"creates a vector of program-fitness structures and fills the program field with programs generated from rhah"
   (into [] (map #(ProgramFitness. % nil nil nil nil) (rhah functions function-args terminals population-size max-tree-size) ))) 
-
-(defn raw-fitnes [programs fitness-function] 
-  "evaluates the raw fitness of each program using the fitness function"
+  
+(defn raw-fitness [programs fitness-function] 
+"evaluates the raw fitness of each program using the fitness function"
   (into [] (map #(assoc % :raw (fitness-function (get % :program))) programs)))
-
-(defn stdfitness [rawProgs bestValue]
-  (into [] (map #(assoc % :std (abs (- bestValue (get % :rawProg))) rawProgs))))
-
-
+  
+(defn standard-fitness [programs bestValue]
+"calculates standard fitness, closer to zero is better. bestvalue is the best possible score the 
+  fitness function can give.  If lower scores are better, use 0. if the highest score is unknown, 
+  use an arbitrarily high value"
+  (into [] (map #(assoc % :std (abs (- bestValue (get % :program))) programs))))
+  
+(defn adjusted-fitness [programs]
+"fills in the adjusted fitness of programs. adjusted fitness is 1/(1+standard)"
+  (into [] (map #(assoc % :adjusted (/ 1 (+ 1 (get % standard)))) programs)))
+  
+(defn normalized-fitness [programs]
+"fills in the normalized fitness of programs.  normalized fitness = adjusted/(sum of all adjusted)"
+  (let [sum (fold + (map #(get % :adjusted) programs))]
+    (into [] (map #(assoc % :normalized (/ (get % :adjusted) sum))) programs)))
+    
+(defn pick-individual [progs]
+  (let [rnd (rand 1.0)]
+    (let [idx (get progs 0)]
+      (loop [i rnd]
+        (when (>= i 0)
+          (dec idx)
+          (recur (- i (program-fitness-nrm (get progs idx)))))
+        )
+      (get progs idx))))
+      
 (defn -main
-  "I don't do a whole lot ... yet."
+"I don't do a whole lot ... yet."
   [& args]
   (let [functions '[+ - *] function-args '[2 2 2] terminals '[1 2 3 4 5 6 7 8 9]]
         (let [funs (gen-zero functions function-args terminals 50 6)]
