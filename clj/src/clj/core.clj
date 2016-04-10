1cde61ab73cf2d2b0e0f50370bc547014ebb328c
jsandler18 <jsandler18@gmail.com>
2016-04-09 21:46:17 -0400

    wrote mutae

--- clj/src/clj/core.clj
+++ clj/src/clj/core.clj
@@ -88,8 +88,8 @@
     (inc (apply max (map height sub-trees)))
     1))
 
-(defn get-cross-point [lst]
- (let [queue2 clojure.lang.PersistentQueue/EMPTY]
+(defn get-cross-point [lst keep-short]
+ (let [keep-short (if (> 0 keep-short) 2 1) queue2 clojure.lang.PersistentQueue/EMPTY]
    (do
      (def winner lst)
      (def n 2)
@@ -101,12 +101,12 @@
           (do
            (def resthead (rest head))
            (def queue (addListToQueue resthead queue))
-           (if (< (rand(- (* 10 n) 1)) 9)
+           (if (< (rand(- (* (* 10 n) keep-short) 1)) 9)
              (def winner head)
              )
            )
           (do
-            (if (< (rand (- (* 10 n) 1)) 1)
+            (if (< (rand (- (* (* 10 n) keep-short) 1)) 1)
             (def winner head)
             )
           )
@@ -125,14 +125,25 @@
   (map #(if (= % old-subtree)
     new-subtree
     (if (seq? %)
-      (set-nth-subtree new-subtree old-subtree %)
+      (set-subtree new-subtree old-subtree %)
       %)) program))
 
 (defn crossover [parent1 parent2]
   "takes 2 parents and performs crossover on them, yeilding 2 new children, which will be returned in a list of 2 elements"
   (let [too-tall (- (+ (height parent1) (height parent2)) 20)];if the sum of the heights are above 20, too-tall wil be > 0, and get-rand will make it more likely to pick from the top
-    (let [cross1 (get-cross-point parent1) cross2 (get-cross-point parent2)]
+    (let [cross1 (get-cross-point parent1 too-tall) cross2 (get-cross-point parent2 too-tall)]
       ((set-subtree cross2 cross1 parent1)) (set-subtree cross1 cross2 parent2))))
+
+(defn mutate [program functions function-args terminals]
+  "does a point mutation on the parent"
+  (let [mutate-subtree (get-cross-point program) mutate-size (+ (rand-int 8) 1)]
+    (map #(if (= % mutate-subtree)
+    (if (= (rand-int 1) 0)
+      (full functions function-args terminals 1 mutate-size)
+      (grow functions function-args terminals 1 mutate-size))
+    (if (seq? %)
+      (mutate %)
+      %)) program)))
       
 (defn -main
 "I don't do a whole lot ... yet."
